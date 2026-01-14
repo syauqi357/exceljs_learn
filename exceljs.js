2a16f9b42171dc43888be18bde80385fcf730825
syauqi <m.syauqi357@gmail.com>
2026-01-14 22:08:54 +0700

    add : new data using add row

---
+++ exceljs.js
@@ -0,0 +1,41 @@
+import Exceljs from "exceljs";
+
+async function createExcelFile() {
+// create new work book
+    const workbook = new Exceljs.Workbook();
+
+    workbook.calcProperties.fullCalcOnLoad =true;
+
+    workbook.views = [
+        {
+            x: 0,
+            y: 0,
+            width: 10000,
+            height: 20000,
+            firstSheet: 0,
+            activeTab: 1,
+            visibility: 'visible'
+        }
+    ]
+
+    const sheet = workbook.addWorksheet('student')
+
+// Add header row
+    sheet.addRow(["ID", "Name", "Age"]);
+
+// Add data rows
+    sheet.addRow([1, "Alice", 23]);
+    sheet.addRow([2, "Bob", 25]);
+    sheet.addRow([3, "Charlie", 22]);
+
+// Save the workbook to a file
+    await workbook.xlsx.writeFile("students.xlsx");
+    console.log("File saved as 'students.xlsx'");
+
+};
+
+createExcelFile();
+
+
+
+
